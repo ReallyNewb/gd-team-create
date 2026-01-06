ead5761febceb4e9a77f09df965d5f860c9deaeb
ReallyNewb <bradenlebron@icloud.com>
2026-01-06 12:37:56 -0600

    stuff

--- README.md
+++ README.md
@@ -1,10 +1,10 @@
-# <cf>Team Create</c>
+# <cl>Team Create</c>
 
 Are you sometimes <cr>tired</c> of the <ca>inefficient method of collaboration,</c> where you simply spam upload <cb>copyables</c> and <cl>continuously edit sections back and forth?</c>
 
 Well, I've got the <cj>solution</c> for you! Uhh... <ca>*Team Create!*</c> A mod that allows for <cy>collaboration with other players!</c> Simply <cr>invite your creator friends</c> to a level, set their <cp>permissions,</c> and have a great time editing together!
 
-# <co>Notes:</c>
+# <cl>Notes:</c>
 
 - <cr>**If you are looking for a much more stable collaboration experience, consider buying the mod [Editor Collab](mod:alk.editor-collab) by alk. While I cannot state any pros and cons between Editor Collab and my mod since I don't have Editor Collab, definitely do compare this mod and Editor Collab to see any (probably noticeable) differences between the two.**</c>
 
@@ -21,6 +21,5 @@ A <cp>system message</c> will <ca>appear,</c> as well as a <cf>user circle that 
 You and the collaborators can **<cf>place</c> <cl>blocks,</c> <cb>triggers,</c> <cp>change settings of the level,</c> and <ca>much more with your creators!</c>**
 
 Based on the <cb>permissions</c> <cl>you gave them,</c> they're <cr>limited</c> as to <cf>what they can do.</c> For example, you <ca>cannot save changes to a level</c> if you are a <cp>"Minimal Collaborator".</c>
-
 
 <cf>As well as all that good stuff,</c> there's a <cy>chat system</c> you can use to <cp>communicate</c> with <co>creators!</c> <cb>Optionally,</c> there's <cf>Discord</c> or <cj>other communication platforms,</c> but this <cr>serves as a half-decent alternative to them.</c>
--- src/utils/argon.hpp
+++ src/utils/argon.hpp
@@ -102,14 +102,14 @@ namespace argonutils {
 
 			"# Privacy Policy\n"
 
-			"## Data We Collect\n"
+			"## <cj>Data We Collect</c>\n"
 
 			"The data we collect are your <cl>username, accountID, userID, Argon token, icon colors, primary icon type and index, and user color.</c>\n"
 			"The first 4 pieces of data are <cp>merely for Argon verification.</c> The rest are <ca>merely cosmetic</c> and <cr>won't share anything personal about your account.</c>\n\n"
 
 			"To make support for the <cr>Friends Only</c> invite mode, we send your <cl>friends list</c> to the server.</c>\n\n"
 
-			"## Data Usage\n"
+			"## <cj>Data Usage</c>\n"
 
 			"<cj>Level data</c> from our server are at times used for training a <cl>Zstd dictionary.</c> "
 			"Note that <cl>Zstd dictionaries</c> are <co>entirely local</c> and don't rely on any external services.\n\n"
@@ -118,22 +118,22 @@ namespace argonutils {
 			"to comply with <cf>disk space limits</c> from the host of the mod's server, [Spaceify.](https://spaceify.eu)\n\n"
 			"By training <cl>Zstd dictionaries,</c> compressing can be made <cj>**MUCH faster.**</c>\n\n"
 
-			"### Data We (Never Will) Share\n"
+			"### <cj>Data We (Never Will) Share</c>\n"
 
 			"The data we collect <cr>is NOT</c> shared with any <cb>third party services.</c> The data is only kept in <cl>Team Create's database</c> and is <cr>**not distributed**</c> to <cb>other databases or random people.</c>\n"
 
-			"## Data Security\n"
+			"## <cj>Data Security</c>\n"
 
 			"We try to <cy>protect your data</c> by using some basic <cr>security methods</c> like <cl>verifying the data sent by the client</c> and <cj>parameterized queries</c> in <cb>SQL commands.</c>\n"
 			"However, there may be <cr>security bugs</c> unnoticed, or <cp>I implemented a security method **really poorly.**</c>\n"
 			"### <cy>If you ever find a security bug, PLEASE and DO send it to me (ReallyNewb) privately via the Geometry Dash messaging system.</c>\n"
 
-			"## User Rights\n"
+			"## <cj>User Rights</c>\n"
 
 			"<cg>Users</c> may <cr>manage their data</c> at anytime using the [mod's settings.](mod:reallynewb.team-create) If you have <co>any suggestion</c> for <cl>data management,</c>"
-			"definitely <cy>post it</c> in the <cl>mod's comments.</c>\n"
+			" definitely <cy>post it</c> in the <cl>mod's comments.</c>\n"
 
-			"## Data Retention\n"
+			"## <cj>Data Retention</c>\n"
 
 			"<cg>A user's data</c> is <cr>retained</c> as long as they <co>delete their account</c> via the [mod's settings.](mod:reallynewb.team-create) <cf>Deleting your account</c> will take up to <cy>14 days</c>"
 			" <cb>just in case</c> you <cl>might've tapped too many buttons.</c>\n\nAs always, <cr>account deletion</c> is <ca>**ALWAYS revertable.**</c>\n"
